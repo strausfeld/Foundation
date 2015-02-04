444681c6545ee9569cd0ac3a2e47e99ba673e27c
Jeremy Zilar <jeremyzilar@gmail.com>
2015-02-04 16:44:47 -0500

    more Examples

--- index.php
+++ index.php
@@ -3,22 +3,7 @@
 	<section>
 		<div class="container">
 
-			<div class="row">
-				<div class="col-xs-12 col-sm-3">
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore</p>
-				</div>
-				<div class="col-xs-12 col-sm-3">
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore</p>
-				</div>
-				<div class="col-xs-12 col-sm-3">
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore</p>
-				</div>
-				<div class="col-xs-12 col-sm-3">
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore</p>
-				</div>
-			</div>
-
-			<div class="row">
+			<div id="main" class="row">
 				<div class="col-xs-12">
 					<p>A — Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 				</div>
@@ -27,51 +12,29 @@
 					<p>B — Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 				</div>
 			</div>
-		</div>
-	</section>
 
-
-	<section id="main">
-		<div class="container">
 			<div class="row">
-				<div class="col-xs-12">
-					<h2>Heading</h2>
-
-					<table class="table table-green">
-						<tr>
-							<th>Head</th>
-							<th>Head</th>
-							<th>Head</th>
-							<th>Head</th>
-						</tr>
-						<tr>
-							<td>item</td>
-							<td>item</td>
-							<td>item</td>
-							<td>item</td>
-						</tr>
-						<tr>
-							<td>item</td>
-							<td>item</td>
-							<td>item</td>
-							<td>item</td>
-						</tr>
-						<tr>
-							<td>item</td>
-							<td>item</td>
-							<td>item</td>
-							<td>item</td>
-						</tr>
-					</table>
-
-					<p>Lorem <mark>highlight</mark> ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<div class="col-xs-12 col-sm-3">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore</p>
+				</div>
+				<div class="col-xs-12 col-sm-3">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore</p>
+				</div>
+				<div class="col-xs-12 col-sm-3">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore</p>
+				</div>
+				<div class="col-xs-12 col-sm-3">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore</p>
 				</div>
 			</div>
+
+			<div class="row">
+				<div class="col-xs-12">
+					<p>Bootstrap — 
+				</div>
+			</div>
+
+
 		</div>
 	</section>
 
